4bab74f30d0b88d1595676980eb0b0062ca3e835
Morten Jensen <mortenhenningjensen@hotmail.com>
2017-05-18 21:38:46 +0200

    Gyro stabilizer made, should work smoother and reset Y axis (Might need twerking, or just plain remove)

--- Gyro Test/Gyro Test/Assets/Gyro.cs
+++ Gyro Test/Gyro Test/Assets/Gyro.cs
@@ -10,38 +10,62 @@ public class Gyro : MonoBehaviour
     float speed;
     public Rigidbody rb;
     public typetorotate type;
+    public Transform mytransform;
+
+    public float initialOrientationX;
+    public float initialOrientationY;
+
+    public float smooth = 0.5F;
+    public float tiltAngle = 30.0F;
+
+    Quaternion startOffset;
 
     // Use this for initialization
     void Start()
     {
+        Screen.orientation = ScreenOrientation.Portrait;
+        if (!Input.gyro.enabled)
+        {
+            Input.gyro.enabled = true;
+        }
+
+        if (GetComponent<Rigidbody>() != null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        startOffset = Input.gyro.attitude;
         speed = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if (mytransform.rotation.y >= 5 || mytransform.rotation.y <= 5)
+        // {
+        //  ResetGyro();
+        //    Input.gyro.enabled = false;
+        //    var rotationsVector = mytransform.rotation.eulerAngles;
+        //    rotationsVector.y = 0;
+        //    mytransform.rotation = Quaternion.Euler(rotationsVector);
+        //    Input.gyro.enabled = true;
+        // }
 
-        //var dir = Vector3.zero;
+        //float xRot = Mathf.Clamp(mytransform.rotation.x, minX, maxX);
+        //float zRot = Mathf.Clamp(mytransform.rotation.z, minY, maxY);
+        //float yRot = Mathf.Clamp(mytransform.rotation.y, 0, 0);
 
-        //dir.x = Input.acceleration.x;
-        //dir.y = Input.acceleration.y;
-        //dir.z = Input.acceleration.z;
-
-
-        //if (dir.sqrMagnitude > 1)
-        //{
-        //    dir.Normalize();
-        //}
-
-        //dir *= Time.deltaTime;
-        //transform.Rotate(dir * speed);
+        //mytransform.rotation = new Quaternion(xRot, yRot, zRot, 0);
+        //   if (mytransform.rotation.y > 0.0003f)
+        // {
+        // ResetGyro();
+        //  }
     }
 
     void FixedUpdate()
     {
-        Input.gyro.enabled = true;
-        float initialOrientationX = Input.gyro.rotationRateUnbiased.x;
-        float initialOrientationY = Input.gyro.rotationRateUnbiased.y;
+        initialOrientationX = -Input.gyro.rotationRateUnbiased.x * tiltAngle;
+        initialOrientationY = -Input.gyro.rotationRateUnbiased.y * tiltAngle;
 
         switch (type)
         {
@@ -49,10 +73,18 @@ public class Gyro : MonoBehaviour
                 rb.AddForce(initialOrientationY * speed, 0.0f, -initialOrientationX * speed);
                 break;
             case typetorotate.level:
-                transform.Rotate(initialOrientationX, 0.0f, initialOrientationY);
+                //mytransform.Rotate(-Input.gyro.gravity.x, 0, -Input.gyro.gravity.y);
+                Quaternion target = Quaternion.Euler(initialOrientationX, 0, initialOrientationY);
+                mytransform.rotation = Quaternion.Slerp(mytransform.rotation, target, Time.deltaTime * smooth);
+                //mytransform.Rotate(initialOrientationX, 0, initialOrientationY);
                 break;
         }
 
+    }
 
+    void ResetGyro()
+    {
+        Quaternion test = new Quaternion(mytransform.eulerAngles.x, 0, mytransform.eulerAngles.z, 0);
+        mytransform.rotation = test;
     }
 }
