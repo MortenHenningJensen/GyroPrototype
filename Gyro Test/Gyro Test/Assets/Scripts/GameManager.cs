cf27b41e283da8d4cbf8341271873206e716a11e
Thomas Marinussen <mer785@hotmail.com>
2017-05-22 19:53:52 +0200

    Plates

---
+++ Gyro Test/Gyro Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private Plate p;
+
+    [SerializeField]
+    private int _numbOfWinPlates; //Test
+    [SerializeField]
+    private int _numbOfActivatedPlates; //Test
+
+    [SerializeField]
+    private GameObject[] plates; //Finds all Gameobjects with tag "Plate"
+    [SerializeField]
+    private List<GameObject> allPlates; //Contains every Plate in game
+    [SerializeField]
+    private List<GameObject> normPlates; //Contains all the activationPlates
+    [SerializeField]
+    private List<GameObject> actPlates; //Contains all the activationPlates
+    [SerializeField]
+    private List<GameObject> goalPlate; //Contains only the Start-/GoalPlate
+
+    [SerializeField]
+    private bool _canEnd; //End condition has been achieved
+
+    public List<GameObject> AllPlates
+    {
+        get
+        {
+            return allPlates;
+        }
+
+        set
+        {
+            allPlates = value;
+        }
+    }
+
+    public List<GameObject> NormPlates
+    {
+        get
+        {
+            return normPlates;
+        }
+
+        set
+        {
+            normPlates = value;
+        }
+    }
+
+    public List<GameObject> ActPlates
+    {
+        get
+        {
+            return actPlates;
+        }
+
+        set
+        {
+            actPlates = value;
+        }
+    }
+
+    public List<GameObject> GoalPlate
+    {
+        get
+        {
+            return goalPlate;
+        }
+
+        set
+        {
+            goalPlate = value;
+        }
+    }
+
+    public int NumbOfActivatedPlates
+    {
+        get
+        {
+            return _numbOfActivatedPlates;
+        }
+
+        set
+        {
+            _numbOfActivatedPlates = value;
+        }
+    }
+
+    public int NumbOfWinPlates
+    {
+        get
+        {
+            return _numbOfWinPlates;
+        }
+
+        set
+        {
+            _numbOfWinPlates = value;
+        }
+    }
+
+    public bool CanEnd
+    {
+        get
+        {
+            return _canEnd;
+        }
+
+        set
+        {
+            _canEnd = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        _numbOfActivatedPlates = 0;
+        allPlates = new List<GameObject>();     //List of all Plates ingame
+        actPlates = new List<GameObject>();     //List of all Activation Plates
+        normPlates = new List<GameObject>();    //List of all Normal Plates
+        goalPlate = new List<GameObject>();     //List containing the Goal Plate
+
+        if (plates == null)
+        {
+            plates = GameObject.FindGameObjectsWithTag("Plate");
+        }
+        foreach (GameObject pl in plates)
+        {
+            allPlates.Add(pl); //adds every gameobject with Tag "Plate" to this list.
+        }
+
+        StartGame();
+
+        NumbOfWinPlates = actPlates.Count; //Number of activation plates to win
+    }
+
+    void StartGame()
+    {
+        foreach(GameObject pl in allPlates)
+        {
+            p.SetupPlates(pl);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        WinningCondition();
+	}
+
+    /// <summary>
+    /// Checks the number of activated plates, if it is equal to 
+    /// the number of plates required to win..
+    /// Disables the triggers on the activationPlates, so you don't accidently 
+    /// ruin the win-condition..
+    /// Changes the material on the StartPlate, to EndPlate..
+    /// </summary>
+    public void WinningCondition()
+    {
+        /*Hele denne metode skal måske flyttes, så det ikke er hver plate der har denne metode,
+        men at det er en del af spillets logik istedet.. */
+        if (_numbOfActivatedPlates == NumbOfWinPlates)
+        {
+            _canEnd = true;
+            foreach (GameObject pl in actPlates)
+            {
+                pl.GetComponent<BoxCollider>().isTrigger = false;
+            }
+            foreach(GameObject pl in goalPlate)
+            {
+                pl.GetComponent<Plate>().CurrentMaterial = pl.GetComponent<Plate>().Material5;
+                pl.GetComponent<Plate>().Rend.material = pl.GetComponent<Plate>().CurrentMaterial;
+            }
+
+            Debug.Log("You can now finish the game!");
+        }
+    }
+}
---
+++ Gyro Test/Gyro Test/Assets/Scripts/Plate.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour {
+
+    public enum PlateType { NormalPlate = 1, ActivationPlate = 2, GoalPlate = 3};
+
+    private GameManager gm;
+
+    [SerializeField]
+    private PlateType _typeNumb; //Type to choose from, in the Inspector
+
+    //[SerializeField]
+    //private int _numbOfWinPlates; //Test
+    //[SerializeField]
+    //private int _numbOfActivatedPlates; //Test
+
+    ////[SerializeField]
+    ////private List<Plate> plates;
+    ////[SerializeField]
+    ////private List<Plate> actPlates;
+    ////[SerializeField]
+    //private List<Plate> goalPlate;
+
+    [SerializeField]
+    private bool _light; //Plate_Activation
+
+    private Renderer _rend;
+
+    [SerializeField]
+    private Material _currentMaterial;
+
+    [SerializeField]
+    private Material _material1;
+    [SerializeField]
+    private Material _material2;
+    [SerializeField]
+    private Material _material3;
+    [SerializeField]
+    private Material _material4;
+    [SerializeField]
+    private Material _material5;
+
+    public Material CurrentMaterial
+    {
+        get
+        {
+            return _currentMaterial;
+        }
+
+        set
+        {
+            _currentMaterial = value;
+        }
+    }
+
+    public Material Material1
+    {
+        get
+        {
+            return _material1;
+        }
+
+        set
+        {
+            _material1 = value;
+        }
+    }
+
+    public Material Material2
+    {
+        get
+        {
+            return _material2;
+        }
+
+        set
+        {
+            _material2 = value;
+        }
+    }
+
+    public Material Material3
+    {
+        get
+        {
+            return _material3;
+        }
+
+        set
+        {
+            _material3 = value;
+        }
+    }
+
+    public Material Material4
+    {
+        get
+        {
+            return _material4;
+        }
+
+        set
+        {
+            _material4 = value;
+        }
+    }
+
+    public Material Material5
+    {
+        get
+        {
+            return _material5;
+        }
+
+        set
+        {
+            _material5 = value;
+        }
+    }
+
+    public PlateType TypeNumb
+    {
+        get
+        {
+            return _typeNumb;
+        }
+
+        set
+        {
+            _typeNumb = value;
+        }
+    }
+
+    public Renderer Rend
+    {
+        get
+        {
+            return _rend;
+        }
+
+        set
+        {
+            _rend = value;
+        }
+    }
+
+    // Use this for initialization
+    public void Start()
+    {
+        ////plates = new List<Plate>();
+
+        Rend = this.GetComponent<Renderer>();
+        Rend.enabled = true;
+        CurrentMaterial = this.GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && this.TypeNumb == PlateType.ActivationPlate)
+        {
+            ChangeLight();
+        }
+    }
+
+    /// <summary>
+    /// Setup the plates and gives them each the various colors and functions
+    /// </summary>
+    public void SetupPlates(GameObject go)
+    {
+        if (go.GetComponent<Plate>().TypeNumb == 0)
+        {
+            go.GetComponent<Plate>().TypeNumb = PlateType.NormalPlate;
+        }
+
+        switch (TypeNumb)
+        {
+            case PlateType.NormalPlate:
+                CurrentMaterial = Material1;
+                //Rend.material = CurrentMaterial;
+                gm.NormPlates.Add(go);
+                break;
+            case PlateType.ActivationPlate:
+                CurrentMaterial = Material2;
+                //Rend.material = CurrentMaterial;
+                _light = false; //Turns all lights off at the start
+                gm.AllPlates.Add(go);
+                break;
+            case PlateType.GoalPlate:
+                CurrentMaterial = Material4;
+                //Rend.material = CurrentMaterial;
+                gm.CanEnd = false; //Sets the goal to false, so you can't end right away
+                gm.AllPlates.Add(go);
+                break;
+            default:
+                break;
+        }
+        Rend.material = CurrentMaterial;
+    }
+
+    /// <summary>
+    /// Changes the light "On" and "Off", and the color of the Plate...
+    /// </summary>
+    void ChangeLight()
+    {
+        _light = !_light; //Switched the current light to the opposite
+        if (_light)
+        {
+            CurrentMaterial = Material3;
+            Rend.material = CurrentMaterial;
+            this._light = true;
+            //gm.NumbOfActivatedPlates++;
+        }
+        else if (!_light)
+        {
+            CurrentMaterial = Material2;
+            Rend.material = CurrentMaterial;
+            this._light = false;
+            //gm.NumbOfActivatedPlates--;
+        }
+
+        //Debug.Log(gm.NumbOfActivatedPlates + " out of " + gm.NumbOfWinPlates);
+        Debug.Log("Plate is now " + _light);
+
+        //gm.WinningCondition(); //Checks if we can win...
+    }
+
+    /// <summary>
+    /// When the ball enters an ActivationPlate
+    /// </summary>
+    /// <param name="ballCol">The ball collider</param>
+    void OntriggerEnter(Collider ballCol)
+    {
+        if (ballCol == GameObject.FindGameObjectWithTag("Ball").GetComponent<Collider>())
+        {
+            //if (this._typeNumb == Type.ActivationPlate && (TouchInput) <-- Brug den her!
+            if (this.TypeNumb == PlateType.ActivationPlate && 
+                this.GetComponent<BoxCollider>().isTrigger == true)
+            {
+                ChangeLight();
+            }
+        }
+    }
+}
