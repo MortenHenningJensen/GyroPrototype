ac800c2e970a9d6acddedc9f48b794425dc2b47b
Morten Jensen <mortenhenningjensen@hotmail.com>
2017-05-26 13:18:11 +0200

    Updated Enemy prefab, to account for collision with player. Made a level with the enemy in it. "Fixed" a bug, where the ball would sometimes not roll in the beginning (fixed poorly with an invisible "rollstarter" collision box Made a roof on bounce level Made some more prefabs, and put them in folders

---
+++ Gyro Test/Gyro Test/Assets/Scripts/RollStarter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollStarter : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            StartCoroutine(ToDestroy());
+        }
+    }
+
+    IEnumerator ToDestroy()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.gameObject);
+    }
+}
